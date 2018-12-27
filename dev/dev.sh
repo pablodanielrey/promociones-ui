ac1c051c29da7fbaa0666efda6fea71eae846146
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-12-27 00:35:04 -0300

    agregue para la autentificación de firebase y poder hacer el deploy y mantener un proyecto

--- dev/dev.sh
+++ dev/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "corriendo en el puerto 20105"
-docker run --rm -ti --name promociones-ui -v $(pwd)/src:/src -p 20105:4200 desarrollo-ui
+docker run --rm -ti --name promociones-ui -v $(pwd)/src:/src -v /src/firebase/promociones:/firebase -p 9005:9005 -p 20105:4200 desarrollo-ui
